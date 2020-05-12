2d2e0e548269399124351354692ed87d20e49f95
Luckyaremu <aremu.baba70@gmail.com>
2020-05-12 17:37:17 +0100

    loop fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :tweeets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root "tweeets#index"
+  root 'tweeets#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,12 +12,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_09_173025) do
-
-  create_table "tweeets", force: :cascade do |t|
-    t.text "tweeet"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 20_200_509_173_025) do
+  create_table 'tweeets', force: :cascade do |t|
+    t.text 'tweeet'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
