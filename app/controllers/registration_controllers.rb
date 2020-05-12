c180bb444063356d00ce0654d73b064e94912197
Luckyaremu <aremu.baba70@gmail.com>
2020-05-12 20:03:10 +0100

    designed the pages

---
+++ app/controllers/registration_controllers.rb
@@ -0,0 +1,9 @@
+class Registrationcontroller < Devise::Registrationcontroller
+    private
+    def sign_up_params
+        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+    end
+    def account_update_params
+        params.require (:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+    end
+end
--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -2,6 +2,7 @@
 
 class TweeetsController < ApplicationController
   before_action :set_tweeet, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /tweeets
   # GET /tweeets.json
