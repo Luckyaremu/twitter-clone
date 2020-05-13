803fea85207cd52f5734ffc994f57c661b685011
Luckyaremu <aremu.baba70@gmail.com>
2020-05-13 12:42:48 +0100

    typo errors in tweeets controller fixed

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -26,8 +26,7 @@ class TweeetsController < ApplicationController
   # POST /tweeets
   # POST /tweeets.json
   def create
-    @tweeet = current.user.tweeets.build(tweeet_params)
-
+    @tweeet = current_user.tweeets.build(tweeet_params)
     respond_to do |format|
       if @tweeet.save
         format.html { redirect_to root_path, notice: 'Tweeet was successfully created.' }
